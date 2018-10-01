10445f50a76524b042f20bb1e47758b3b2b81318
Emma Albury <emma_albury@hotmail.com>
2018-10-01 17:19:28 +0100

    wrote Decrease, increase, power saving mode

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -13,4 +13,26 @@ describe("Thermostat", function() {
     ts.increaseTemp()
     expect(ts.temperature).toEqual(21)
   });
+
+  it("should decrease temperature after the down function is called", function() {
+    ts.decreaseTemp()
+    expect(ts.temperature).toEqual(19)
+  });
+
+  it("should not reduce temperature below 10", function() {
+    var step;
+    for (step = 0; step < 11; step++) {
+      ts.decreaseTemp();
+    }
+    expect(ts.temperature).toEqual(10)
+  });
+
+  it("Has power saving mode on by default", function() {
+    expect(ts.powerSaving).toEqual("on")
+  });
+
+  it("Should be able to turn off power saving", function() {
+    ts.flipPowerSaving()
+    expect(ts.powerSaving).toEqual("off")
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,7 +2,24 @@ function Thermostat() {
 }
 
 Thermostat.prototype.temperature = 20
+Thermostat.prototype.powerSaving = "on"
 
 Thermostat.prototype.increaseTemp = function() {
   this.temperature += 1;
+}
+
+Thermostat.prototype.decreaseTemp = function () {
+  this.temperature -= 1;
+  if(this.temperature < 10) {
+    this.temperature = 10;
+  }
+}
+
+Thermostat.prototype.flipPowerSaving = function () {
+  if (this.powerSaving == "on") {
+    this.powerSaving = "off";
+  }
+  else {
+    this.powerSaving = "on";
+  }
 }
