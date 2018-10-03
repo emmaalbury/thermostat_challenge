04fb0c9ef76c1fc17981bd0c1d9645ac0b7724b8
Emma Albury <emma_albury@hotmail.com>
2018-10-03 17:23:22 +0100

    API

--- interface.js
+++ interface.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
   updateTemperature();
+  displayWeather('London');
+
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    displayWeather(city);
+  });
+
 
   $('#temperature-up').click(function() {
     thermostat.increaseTemp();
@@ -29,6 +37,13 @@ $(document).ready(function() {
     updateTemperature();
   });
 
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+    $('#current-temperature').text(data.main.temp);
+  });
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.energyUsage());
