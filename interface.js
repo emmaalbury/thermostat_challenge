291ad6fb47311c47f757ed1fbcfdf04ea208d5b3
Emma Albury <emma_albury@hotmail.com>
2018-10-04 14:38:08 +0100

    fixed bug in interface

--- interface.js
+++ interface.js
@@ -42,8 +42,10 @@ $(document).ready(function() {
     var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
     var units = '&units=metric';
     $.get(url + token + units, function(data) {
-    $('#current-temperature').text(data.main.temp);
-  });
+      $('#current-temperature').text(data.main.temp);
+    });
+  }
+
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.energyUsage());
